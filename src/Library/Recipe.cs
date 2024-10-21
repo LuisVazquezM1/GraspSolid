d5dc39e988acc8ae95bcdc5ee80a2b34547a8104
LuisVazquezM1 <luis.vazquez@correo.ucu.edu.uy>
2024-10-21 09:57:24 -0300

    Update

---
+++ src/Library/Recipe.cs
@@ -0,0 +1,60 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+
+namespace Full_GRASP_And_SOLID
+{
+    public class Recipe : IPrintable
+    {
+        private IList<BaseStep> steps = new List<BaseStep>();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Product input, double quantity, Equipment equipment, int time)
+        {
+            Step step = new Step(input, quantity, equipment, time);
+            this.steps.Add(step);
+        }
+
+        public void AddStep(string description, int time)
+        {
+            WaitStep step = new WaitStep(description, time);
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(BaseStep step)
+        {
+            this.steps.Remove(step);
+        }
+
+        public string GetTextToPrint()
+        {
+            string result = $"Receta de {this.FinalProduct.Description}:\n";
+            foreach (BaseStep step in this.steps)
+            {
+                result = result + step.GetTextToPrint() + "\n";
+            }
+
+            result = result + $"Costo de producción: {this.GetProductionCost()}";
+
+            return result;
+        }
+
+        public double GetProductionCost()
+        {
+            double result = 0;
+
+            foreach (BaseStep step in this.steps)
+            {
+                result = result + step.GetStepCost();
+            }
+
+            return result;
+        }
+    }
+}
